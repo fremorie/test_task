b86ee72cc8d80a3b4be86d2aabe51393043daad4
fremorie <daria.smirnova.ig@gmail.com>
2018-09-05 17:50:18 +0300

    add mixins

--- draft_scss/js/main.js
+++ draft_scss/js/main.js
@@ -6,9 +6,9 @@ function setNumbers() {
   var num = getRandomInt(100);
   document.querySelector('#numberOfPacks').innerHTML = num;
   (num < 10) ? num = '00' + num : num = '0' + num;
-  document.querySelector('.digit_left').innerHTML = num[0];
-  document.querySelector('.digit_center').innerHTML = num[1];
-  document.querySelector('.digit_right').innerHTML = num[2];
+  document.querySelector('.counter__digit_left').innerHTML = num[0];
+  document.querySelector('.counter__digit_center').innerHTML = num[1];
+  document.querySelector('.counter__digit_right').innerHTML = num[2];
 
 }
 
